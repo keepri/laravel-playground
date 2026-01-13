c7f2a7ecae265074e845c1eb0dfdfbea24d2f648
Vlad Plamadeala <vladislav.plamadeala.extern@haufe.com>
2026-01-13 11:31:48 +0200

    fix livewire

--- app/Livewire/Pages/Welcome.php
+++ app/Livewire/Pages/Welcome.php
@@ -4,20 +4,22 @@ declare(strict_types=1);
 
 namespace App\Livewire\Pages;
 
+use Illuminate\Contracts\View\View;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
+#[Layout('layouts.app')]
 class Welcome extends Component
 {
-    public int $initial = 5;
+    public int $initial = 0;
 
-    public function mount(int $initial = 5): void
+    public function mount(int $initial = 8): void
     {
         $this->initial = $initial;
     }
 
-    public function render(): \Illuminate\Contracts\View\View
+    public function render(): View
     {
         return view('livewire.pages.welcome');
     }
 }
-
